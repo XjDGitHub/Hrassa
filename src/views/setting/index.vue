360a6bc8992ef81a7087bbd65d2713cabd24b543
XjDGitHub <1291514712@qq.com>
2023-03-22 19:48:13 +0800

    公司和角色模块完成

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,14 +1,214 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height: 60px">
+              <el-button icon="el-icon-plus" size="small" type="primary"
+                >新增角色</el-button
+              >
+            </el-row>
+            <!-- 表格 -->
+            <el-table border="" :data="list">
+              <el-table-column type="index" label="序号" width="120" />
+              <el-table-column prop="name" label="角色名称" width="240" />
+              <el-table-column prop="description" label="描述" />
+              <el-table-column label="操作">
+                <template slot-scope="{ row }">
+                  <el-button size="small" type="success">分配权限</el-button>
+                  <el-button
+                    size="small"
+                    type="primary"
+                    @click="editRol(row.id)"
+                    >编辑</el-button
+                  >
+                  <el-button
+                    size="small"
+                    type="danger"
+                    @click="deleteRole(row.id)"
+                    >删除</el-button
+                  >
+                </template>
+              </el-table-column>
+            </el-table>
+            <!-- 分页组件 -->
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <!-- 分页组件 -->
+              <el-pagination
+                :total="page.total"
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                layout="prev,pager,next"
+                @current-change="changeSize"
+              />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              type="info"
+              show-icon
+              :closable="false"
+            />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input
+                  disabled
+                  style="width: 400px"
+                  v-model="formData.name"
+                />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input
+                  disabled
+                  style="width: 400px"
+                  v-model="formData.companyAddress"
+                />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input
+                  disabled
+                  style="width: 400px"
+                  v-model="formData.mailbox"
+                />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width: 400px"
+                  v-model="formData.remarks"
+                />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
+    <el-dialog :visible="showDialog" @close="btnCancel">
+      <el-form
+        ref="roleForm"
+        :model="roleForm"
+        :rules="rules"
+        label-width="120px"
+      >
+        <el-form-item label="角色名称" prop="name">
+          <el-input v-model="roleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input v-model="roleForm.description"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- 底部按钮 -->
+      <el-row slot="footer">
+        <el-col :span="6">
+          <el-button size="small" @click="btnCancel">取消</el-button>
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-export default {};
+import {
+  getRoleList,
+  getCompanyInfo,
+  deleteRole,
+  updateRole,
+  getRoleDetail,
+  addRole,
+} from "@/api/setting";
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      list: [],
+      page: {
+        page: 1,
+        pagesize: 10,
+        total: 0,
+      },
+      formData: {},
+      showDialog: false,
+      roleForm: {},
+      rules: {
+        name: [
+          { required: true, message: "角色名称不能为空", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  computed: {
+    ...mapGetters(["companyId"]),
+  },
+  methods: {
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.page);
+      this.page.total = total;
+      this.list = rows;
+    },
+    changeSize(newSize) {
+      this.page.page = newSize;
+      this.getRoleList();
+    },
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId);
+      console.log(this.formData);
+    },
+    async deleteRole(id) {
+      try {
+        await this.$confirm("确定删除该角色么");
+        await deleteRole(id);
+        this.getRoleList();
+      } catch (error) {
+        console.error();
+      }
+    },
+    async editRol(id) {
+      this.roleForm = await getRoleDetail(id);
+      this.showDialog = true;
+      console.log(this.roleForm);
+    },
+    btnCancel() {
+      this.roleForm = {
+        name: "",
+        description: "",
+      };
+      this.$refs.roleForm.resetFields();
+      this.showDialog = false;
+    },
+    async btnOK() {
+      try {
+        await this.$refs.roleForm.validate();
+        if (this.roleForm.id) {
+          await updateRole(this.roleForm);
+        } else {
+          await addRole(this.roleForm);
+        }
+        this.getRoleList();
+        this.$message.success("操作成功");
+        this.showDialog = false;
+      } catch (error) {
+        console.error();
+      }
+    },
+  },
+  created() {
+    this.getRoleList();
+    this.getCompanyInfo();
+  },
+};
 </script>
 
-<style>
+<style scoped>
 </style>
